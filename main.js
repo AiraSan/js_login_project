e26be8ceacc3fe2d4ecf5c84e8af1dd8278131b5
AiraSan <dthar06@gmail.com>
2022-04-16 23:27:04 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,27 @@
+const defaultEmail = "zarmani@gmail.com";
+const defaultPass = "zarmani123";
+
+
+function login(e) {
+
+    e.preventDefault();
+    
+    let email = e.target[0].value;
+    let pass = e.target[1].value;
+
+    if(email && pass){
+        if(email === defaultEmail && pass === defaultPass) {
+            document.querySelector('#success').innerHTML = "Login Successfully!"
+            document.querySelector('.container').style.display = 'none';
+        } 
+        else if (email === defaultEmail && pass !== defaultPass) {
+            document.querySelector('#error').innerHTML = "Wrong Password"
+        } 
+        else if (email !== defaultEmail && pass === defaultPass) {
+            document.querySelector('#error').innerHTML = "Wrong Email"
+        }
+    } 
+    else {
+        document.querySelector('#error').innerHTML = "Please fill out of the fields!"
+    }
+}
